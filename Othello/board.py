cc453779bffa0322f562c6b20347c594a4f94cb4
AbdelrahmanMoawad <90070556+AbdelrahmanMoawad@users.noreply.github.com>
2025-12-14 18:35:34 +0200

    Add board functionality for Othello game
    Implemented board initialization, display, copy, score counting, opponent determination, and bounds checking.

--- Othello/board.py
+++ Othello/board.py
@@ -1,21 +1,44 @@
+from constants import BOARD_SIZE, BLACK, WHITE
+from cell import Cell
+
+
 class Board:
     def __init__(self):
-        pass
+        self.board = [[Cell() for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        self.board[3][3].set_color(WHITE)
+        self.board[3][4].set_color(BLACK)
+        self.board[4][3].set_color(BLACK)
+        self.board[4][4].set_color(WHITE)
 
     def display(self):
-        pass
+        print("   " + " ".join(str(c + 1) for c in range(BOARD_SIZE)))
+        for i in range(BOARD_SIZE):
+            print(str(i + 1) + "  ", end="")
+            for j in range(BOARD_SIZE):
+                print(self.board[i][j].get_color(), end=' ')
+            print()
+        print()
 
     def copy(self):
-        pass
+        new_board = Board()
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                new_board.board[i][j].set_color(self.board[i][j].get_color())
+        return new_board
 
     def count_score(self, player):
-        pass
+        return sum(
+            1
+            for r in range(BOARD_SIZE)
+            for c in range(BOARD_SIZE)
+            if self.board[r][c].get_color() == player
+        )
 
     def opponent_player(self, player):
-        pass
+        return BLACK if player == WHITE else WHITE
 
     def in_bounds(self, row, col):
-        pass
+        return 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE
 
     def is_valid_move(self, row, col, player):
         pass
